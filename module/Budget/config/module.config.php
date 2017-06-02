7601abc5e1ec243d6a15557408506ce43e5c0378
pancho requena <pancheps@gmail.com>
2017-06-02 10:26:12 -0400

    first commit with module

---
+++ module/Budget/config/module.config.php
@@ -0,0 +1,27 @@
+<?php
+namespace Budget;
+
+use Zend\Router\Http\Literal;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'user' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/user',
+                    'defaults' => [
+                        'controller' => Controller\UserController::class,
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'invokables' => [
+            'Budget\Controller\User' => 'Budget\Controller\UserController',
+        ],
+    ],
+];
